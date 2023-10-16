b0103ca89d43b5fe80389c10b2ae59d7d8bc2749
Dalel1akremi <dalelakremi2020@gmail.com>
2023-10-16 18:19:55 +0100

    feat:add `etudiant` module

---
+++ backend/models/Etudiants.js
@@ -0,0 +1,42 @@
+import { Sequelize } from "sequelize";
+import db from "../config/DataBase.js";
+
+const { DataTypes } = Sequelize;
+
+const Etudiant = db.define('Etudiants',{
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true},
+    nom:{
+        type: DataTypes.STRING
+    },
+    prenom:{
+        type: DataTypes.STRING
+    },
+    date_naiss:{
+        type: DataTypes.DATETIME
+    },
+    cin:{
+        type: DataTypes.INTEGER
+    },
+    photo_identite :{
+        type: DataTypes.LONGBLOB
+    },
+    type_mime:{
+        type:DataTypes.STRING
+    },
+    email:{
+        type: DataTypes.STRING,
+        primaryKey: true,
+        
+    },
+    password:{
+        type: DataTypes.STRING
+    }
+},{
+    freezeTableName:true
+});
+
+export default Etudiant;
+
