4cced375cba8f80542a44bfb2d1c1bee60ab2447
Dalel1akremi <dalelakremi2020@gmail.com>
2023-10-16 21:12:24 +0100

    feat: fix `database` sql file

--- backend/DataBase.sql
+++ backend/DataBase.sql
@@ -2,9 +2,9 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
--- Table structure for table `affichage`
+-- Table structure for table affichage
 CREATE TABLE Etudiants (
-   id INT AUTO_INCREMENT PRIMARY KEY,
+   id INT ,
     prenom VARCHAR(50) NOT NULL,
     nom VARCHAR(50) NOT NULL,
     date_naiss DATETIME,
@@ -13,24 +13,24 @@ CREATE TABLE Etudiants (
     password VARCHAR(255),
     photo_identite LONGBLOB,
     type_mime VARCHAR(50),
-    date_inscription DATETIME
+    date_inscription DATETIME,
     createdAt DATETIME NOT NULL,
     updatedAt DATETIME NOT NULL,
    PRIMARY KEY (id, email)
 );
 
--- AUTO_INCREMENT for table `Etudiants`
-ALTER TABLE `Etudiants`
-  MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+-- AUTO_INCREMENT for table Etudiants
+ALTER TABLE Etudiants
+  MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
    
    
-   --enseignants--
-CREATE TABLE Enseingnants (
-   id_ens INT(11) NOT NULL AUTO_INCREMENT,
+  
+CREATE TABLE Enseignants (
+   id_ens INT NOT NULL ,
    nom VARCHAR(50),
    prenom VARCHAR(50),
-   cin in(8),
+   cin int(8),
    module varchar(50),
    coeff float(4),
    email VARCHAR(50) UNIQUE,
@@ -40,7 +40,7 @@ CREATE TABLE Enseingnants (
    PRIMARY KEY (id_ens, email)
 );
 
--- AUTO_INCREMENT for table `users`
-ALTER TABLE `Enseignants`
-  MODIFY `id_ens` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+ALTER TABLE Enseignants
+  MODIFY id_ens INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
