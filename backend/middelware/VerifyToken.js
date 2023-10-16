93fb45abd9ac64123fd4701698ea4f7d9da417a1
Dalel1akremi <dalelakremi2020@gmail.com>
2023-10-16 18:42:46 +0100

    feat:add middleware `verifyToken`

---
+++ backend/middelware/VerifyToken.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+
+
+
+const authHeader = req.headers['authorization'];
+const token = authHeader && authHeader.split(' ')[1];
+
+console.log("data: ", process.env.ACCESS_TOKEN_SECRET)
+  
+  if (token.length === 0) {
+    return res.status(401).json({ message: 'JWT must be provided' });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    
+    if (err) {
+        return res.status(401).json({ message: err.message });
+    } 
+    console.log("user: ", decoded)
+    req.user = decoded; // Attach user information to the request object
+    next();
+  });
+}
