8c78f00e2017655fe4ef7885ff9132f790a04a2f
yakinebenali <yakinebenali5@gmail.com>
2023-11-26 16:40:53 +0100

    fix

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -357,38 +357,44 @@ export const moyenne = async (req, res) => {
       return res.status(404).json({ message: "Aucune note trouvée." });
     }
 
-    // Calculer la moyenne de tous les étudiants
+
     const moyennes_par_matiere = [];
     const moyenneGenerale = {};
+  
+    
 
     allNotes.forEach(note => {
       if (note.Etudiant && note.Etudiant.id) {
         const matiere = note.Matiere;
         const etudiant = note.Etudiant;
         let moyenne;
-
-        if (matiere.type_matiere === 'tp' && note.note_ds1 !== null && note.note_tp !== null && note.note_examen !== null) {
-          // Si le type de matière est 'tp', calculer la moyenne avec la note tp
+    
+        if (matiere.type_matiere === 'tp' && note.note_ds1 !== undefined && note.note_tp !== undefined && note.note_examen !== undefined) {
+         
           moyenne = (note.note_ds1 + note.note_tp + note.note_examen * matiere.coefficient) / (2 + matiere.coefficient);
-        } else if (note.note_ds1 !== null && note.note_examen !== null) {
-          // Sinon, calculer la moyenne sans la note tp
+        } 
+        else if( note.note_ds1 !== undefined  && note.note_examen !== undefined) {
+         
           moyenne = (note.note_ds1 + note.note_examen * matiere.coefficient) / (1 + matiere.coefficient);
-
-          moyennes_par_matiere.push({ id_Etudiant: etudiant.id, nom_Etudiant: etudiant.nom, prenom_Etudiant: etudiant.prenom, nom_matiere: matiere.nom_matiere, moyenne });
-
-          // Calculer la moyenne générale pour chaque étudiant
-          if (!moyenneGenerale[etudiant.id]) {
-            moyenneGenerale[etudiant.id] = { total: 0, count: 0 };
+    
+          
+      
+            }
+            else {
+              res.status(500).json({ message: `Erreur lors du calcul des moyennes, l'étudiant de l'ID ${etudiant.id} a une note manquante.` });
+            }
+    
+            moyennes_par_matiere.push({ id_Etudiant: etudiant.id, nom_Etudiant: etudiant.nom, prenom_Etudiant: etudiant.prenom, nom_matiere: matiere.nom_matiere, moyenne });
+    
+            if (!moyenneGenerale[etudiant.id]) {
+              moyenneGenerale[etudiant.id] = { total: 0, count: 0 };
+            moyenneGenerale[etudiant.id].total += moyenne;
+            moyenneGenerale[etudiant.id].count += 1;
           }
-
-          moyenneGenerale[etudiant.id].total += moyenne;
-          moyenneGenerale[etudiant.id].count += 1;
-        } else {
-          console.error('Erreur lors du calcul des moyennes, une note manquante :', error);
-        }
-      }
-    });
-
+  }});
+    
+    
+   
     // Calculer la moyenne générale finale
     const moyenneGeneraleFinale = [];
     for (const [id, data] of Object.entries(moyenneGenerale)) {
