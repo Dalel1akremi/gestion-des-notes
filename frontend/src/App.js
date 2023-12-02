8ee07ba01bcb38bedb92f41a31f23211a512d1b4
dalel <dalelakremi2020@gmail.com>
2023-12-02 22:56:48 +0100

    feat: fix `App.js`

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
 import Home from "./components/Home";
 import Login from "./components/login";
+import Ajout from "./components/AjoutMatiere";
+
 import StudentGradesTable from "./components/StudentGradesTable";
 
 import './App.css';
@@ -17,6 +19,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/Ajout" element={<Ajout />} />
           <Route path="/register" element={<Register />} />
           <Route path="/grades" element={<StudentGradesTable />} />
           {/* Add more routes as needed */}
