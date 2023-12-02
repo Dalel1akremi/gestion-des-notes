50939819570f046f114e97b5bcbf053701b5b50f
dalel <dalelakremi2020@gmail.com>
2023-12-02 22:15:09 +0100

    feat: fix `app` js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,28 @@
 import React from "react";
-import SignupAdmn from "./components/SignupAdmn";
+import { Fragment } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./components/Register";
+import Home from "./components/Home";
+import Login from "./components/login";
+import StudentGradesTable from "./components/StudentGradesTable";
 
 import './App.css';
 
 function App() {
+  const user = localStorage.getItem("token");
+
   return (
-  
-      <div>
-        <SignupAdmn />
-      </div>
- 
-  
+    <Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/grades" element={<StudentGradesTable />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
   );
 }
 
