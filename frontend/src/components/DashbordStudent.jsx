8f087c9ed3fdbaedac98ffb6a50b770de9a22153
AbirGhrissi <abirghrissi83@gmail.com>
2023-12-23 14:50:03 +0100

    feat:add empty file `DashbordStudent`

---
+++ frontend/src/components/DashbordStudent.jsx
@@ -0,0 +1,9 @@
+import React,{ useState } from "react";
+import axios from "axios";
+
+const DashbordStudent = () => {
+    return(
+        <div>Dashbord Student</div>
+    )
+}
+export default DashbordStudent;
